d19aac51e47156bd2f9a47e9cf0fc67435b2d6f6
Mina Youssef <mina.awadalla70@gmail.com>
2021-10-02 18:07:47 +0300

    url update 2

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://finalproejctexpressapp.herokuapp.com/'
+export const API_URL = 'https://finalproejctexpressapp.herokuapp.com'
 export const REQ_HEADER_CREDENTIALS = 'include'
